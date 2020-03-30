cc48a5515fe4999acd9118a68e628171c1bc02f7
Frank Benus <benusfrank@gmail.com>
2020-03-30 15:22:24 +0200

    Removed crm status

--- models/userprofile.js
+++ models/userprofile.js
@@ -11,8 +11,8 @@ class UserProfile {
                 this.phoneNumber = profileJson.profile.mobilePhone
                 this.email = profileJson.profile.email
                 this.title = profileJson.profile.title
-                this.crmStatus = profileJson.profile.crm_status
-                if (this.mfaPreferred) {
+                this.crmStatus = profileJson.profile.account_type
+                if (profileJson.profile.mfa_preferred) {
                     this.mfaPreferred = profileJson.profile.mfa_preferred    
                 } else {
                     this.mfaPreferred = 'false'
