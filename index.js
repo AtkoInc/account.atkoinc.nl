b5b3982a407108d65f7da2e449e81dc824bc81ef
Frank Benus <benusfrank@gmail.com>
2020-04-03 12:41:35 +0200

    Revert "Complete redo of the app"
    This reverts commit 52cab81e69306f6616c339fef243226705535734.

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const hbs  = require('express-handlebars')
 const session = require('express-session')
 const axios = require('axios')
 const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 var passport = require('passport');
 var logger = require('./logger')
 
 const tenantResolver = require('./tenantResolver')
 const userProfile = require('./models/userprofile')
-const appLink = require('./models/applink')
 
 const PORT = process.env.PORT || 3000;
 
@@ -36,6 +36,11 @@ app.engine('hbs',  hbs( {
             } else {
                 return "Invalid or empty token was parsed"
             }
+        },
+        'select': function(selected, options) {
+            return options.fn(this).replace(
+                new RegExp(' value=\"' + selected + '\"'),
+                '$& selected="selected"');
         }
     }
   } ) );
@@ -103,37 +108,187 @@ function parseError(error){
 
 const router = express.Router();
 
-router.get("/",tr.ensureAuthenticated(), async (req, res, next) => {
-    logger.verbose("/ requested")
-    const requestingTenant = tr.getRequestingTenant(req);
+router.get('/', function(req, res, next) {
+  res.render('index', {layout: 'home', template: 'home'});
+});
 
+router.get("/home",tr.ensureAuthenticated(), async (req, res, next) => {
+    logger.verbose("/home requested")
+    res.render('account_home', {layout: 'main', template: 'account_home'});
+});
+
+router.get('/forgetme', function(req, res, next) {
+  res.render('account_delete', {layout: 'main', template: 'account_delete'});
+});
+
+
+
+router.get("/me",tr.ensureAuthenticated(), async (req, res, next) => {
+    logger.verbose("/me requested")
     const tokenSet = req.userContext.tokens;
     axios.defaults.headers.common['Authorization'] = `Bearer `+tokenSet.access_token
     try {
-        const response = await axios.get(tr.getRequestingTenant(req).tenant+'/api/v1/users/me/appLinks')
-        var apps = [];
-
-        for(var idx in response.data){
-            var app = new appLink(response.data[idx]);
-            apps.push(app);
-        }
-//        console.log(apps);
-        res.render("index",{
+        const response = await axios.get(tr.getRequestingTenant(req).tenant+'/api/v1/users/me')
+        var profile = new userProfile(response.data)
+        res.render("me",{
             tenant: tr.getRequestingTenant(req).tenant,
             tokenSet: req.userContext.tokens,
-            apps: apps
+            user: profile,
         });
     }
     catch(error) {
-        console.log(error);
-         res.render("index",{
-             tenant: tr.getRequestingTenant(req).tenant,
-             tokenSet: req.userContext.tokens,
-             user: new userProfile(),
-             error: parseError(error)
-         });
+        res.render("me",{
+            tenant: tr.getRequestingTenant(req).tenant,
+            tokenSet: req.userContext.tokens,
+            user: new userProfile(),
+            error: parseError(error)
+        });
     }
+});
 
+router.get("/account",tr.ensureAuthenticated(), async (req, res, next) => {
+    logger.verbose("/account requested")
+    const tokenSet = req.userContext.tokens;
+    axios.defaults.headers.common['Authorization'] = `Bearer `+tokenSet.access_token
+    try {
+        const response = await axios.get(tr.getRequestingTenant(req).tenant+'/api/v1/users/me')
+        var profile = new userProfile(response.data)
+        console.log(profile);
+        res.render("account_edit",{
+            tenant: tr.getRequestingTenant(req).tenant,
+            tokenSet: req.userContext.tokens,
+            user: profile,
+        });
+    }
+    catch(error) {
+        res.render("account_edit",{
+            tenant: tr.getRequestingTenant(req).tenant,
+            tokenSet: req.userContext.tokens,
+            user: new userProfile(),
+            error: parseError(error)
+        });
+    }
+});
+
+router.get("/account_profile",tr.ensureAuthenticated(), async (req, res, next) => {
+    logger.verbose("/account profile requested")
+    const tokenSet = req.userContext.tokens;
+    axios.defaults.headers.common['Authorization'] = `Bearer `+tokenSet.access_token
+    try {
+        const response = await axios.get(tr.getRequestingTenant(req).tenant+'/api/v1/users/me')
+        var profile = new userProfile(response.data)
+        console.log(profile);
+        res.render("account_profile",{
+            tenant: tr.getRequestingTenant(req).tenant,
+            tokenSet: req.userContext.tokens,
+            user: profile,
+        });
+    }
+    catch(error) {
+        res.render("account_profile",{
+            tenant: tr.getRequestingTenant(req).tenant,
+            tokenSet: req.userContext.tokens,
+            user: new userProfile(),
+            error: parseError(error)
+        });
+    }
+});
+
+router.post("/account_profile", [tr.ensureAuthenticated(), urlencodedParser], async (req, res, next) => {
+    const tokenSet = req.userContext.tokens;
+    axios.defaults.headers.common['Authorization'] = `Bearer `+tokenSet.access_token
+
+    try {        
+        await axios.post(tr.getRequestingTenant(req).tenant+'/api/v1/users/me', {
+            'profile': {
+                firstName: req.body.first_name,
+                lastName: req.body.last_name,
+                title: req.body.title,
+            }
+        })
+
+        res.redirect('/account_profile');
+    }
+    catch(error) {
+        res.render("/account_profile",{
+            tenant: tr.getRequestingTenant(req).tenant,
+            tokenSet: req.userContext.tokens,
+            user: new userProfile(),
+            error: parseError(error)
+        });
+    }
+});
+
+router.post("/me/change-password",[tr.ensureAuthenticated(), urlencodedParser], async (req, res, next) => {
+    logger.verbose("/password-reset posted")
+    const tokenSet = req.userContext.tokens;
+    axios.defaults.headers.common['Authorization'] = `Bearer `+tokenSet.access_token
+    try {
+        const response = await axios.get(tr.getRequestingTenant(req).tenant+'/api/v1/users/me')
+        var changePasswordLink = response.data._links.changePassword.href;
+
+        var body = {
+            oldPassword: req.body.current_password,
+            newPassword: req.body.password
+        }
+
+        await axios.post(changePasswordLink, body);
+
+        res.redirect('/me');
+    }
+    catch(error) {
+        res.render("change-password",{
+            error: parseError(error)
+        });
+    }
+});
+
+router.get("/me/change-password",tr.ensureAuthenticated(), async (req, res, next) => {
+    logger.verbose("/password-reset requested")
+    const tokenSet = req.userContext.tokens;
+    axios.defaults.headers.common['Authorization'] = `Bearer `+tokenSet.access_token
+    try {
+        const response = await axios.get(tr.getRequestingTenant(req).tenant+'/api/v1/users/me')
+
+        res.render("change-password",{
+            tenant: tr.getRequestingTenant(req).tenant,
+            tokenSet: req.userContext.tokens
+        });
+    }
+    catch(error) {
+        res.render("me",{
+            tenant: tr.getRequestingTenant(req).tenant,
+            tokenSet: req.userContext.tokens,
+            user: new userProfile(),
+            error: parseError(error)
+        });
+    }
+});
+
+router.get("/me/configure-mfa",tr.ensureAuthenticated(), async (req, res, next) => {
+    logger.verbose("/configure-mfa requested")
+    const tokenSet = req.userContext.tokens;
+    axios.defaults.headers.common['Authorization'] = `Bearer `+tokenSet.access_token
+    try {
+        var idToken = parseJWT(req.userContext.tokens.id_token);
+        const enrolled = await axios.get(tr.getRequestingTenant(req).tenant+'/api/v1/users/'+idToken.sub+'/factors');
+        const toEnroll = await axios.get(tr.getRequestingTenant(req).tenant+'/api/v1/users/'+idToken.sub+'/factors/catalog');
+
+        res.render("configure-mfa",{
+            tenant: tr.getRequestingTenant(req).tenant,
+            tokenSet: req.userContext.tokens,
+            enrolledFactors: enrolled.data,
+            factorsToEnroll: toEnroll.data
+        });
+    }
+    catch(error) {
+        res.render("me",{
+            tenant: tr.getRequestingTenant(req).tenant,
+            tokenSet: req.userContext.tokens,
+            user: new userProfile(),
+            error: parseError(error)
+        });
+    }
 });
 
 router.get("/logout", tr.ensureAuthenticated(), (req, res) => {
